ae09a71d42b35e1dd283c03931c635573c3287a0
jwrth <johannes.wirth@helmholtz-muenchen.de>
2021-07-12 09:19:33 +0200

    new subsample script

--- src/fill_barcode_legend.py
+++ src/fill_barcode_legend.py
@@ -4,7 +4,7 @@
 Script to fill the barcode legend for the DbitX toolbox.
 
 Usage:
-	python fill_barcode_legend.py <well_assignment_file.csv> <empty_barcode_legend.csv>
+    python fill_barcode_legend.py <well_assignment_file.csv> <empty_barcode_legend.csv>
 '''
 
 import pandas as pd
@@ -12,27 +12,29 @@ import numpy as np
 import sys
 
 
-## Functions
+# Functions
 def create_pos_to_coord_dict(assignment_dataframe, coord_name):
-    d = {pos: coord for (pos, coord) in zip(assignment_dataframe[coord_name + "_WellPosition"], assignment_dataframe[coord_name + "_Coord"]) if not pd.isnull(pos)}
+    d = {pos: int(coord) for (pos, coord) in zip(
+        assignment_dataframe[coord_name + "_WellPosition"], assignment_dataframe[coord_name + "_Coord"]) if not pd.isnull(pos)}
     return d
 
 def fill_legend(legend, assignment, name):
-    
+
     pos_to_coord = create_pos_to_coord_dict(assignment, name)
-    
+
     new_column = []
-    
+
     for pos in legend["WellPosition"]:
         if pos in pos_to_coord:
             entry = pos_to_coord[pos]
             new_column.append(entry)
         else:
             new_column.append(pd.NA)
-    
+
     legend[name] = new_column
 
-## Script start
+# Script start
+
 
 # Fetch input files
 assignment_file = sys.argv[1]
