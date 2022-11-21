54775df54bd4afab1507963e97bf49e8f31da498
jwrth <johannes.wirth@helmholtz-muenchen.de>
2022-11-21 14:50:52 +0100

    add color palettes

---
+++ xdbit_funcs/plotting/color_palettes.py
@@ -0,0 +1,10 @@
+class color_palettes:
+    '''
+    Class containing a collection of custom color palettes.
+    '''
+    def __init__(self):
+        # palette for colorblind people. From: https://gist.github.com/thriveth/8560036
+        self.colorblind = ['#377eb8', '#ff7f00', '#4daf4a',
+                           '#f781bf', '#a65628', '#984ea3',
+                           '#999999', '#e41a1c', '#dede00']
+        
