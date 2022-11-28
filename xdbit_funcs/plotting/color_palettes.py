bb7835265a266bb43fa910ec4378cb42c58a4809
jwrth <johannes.wirth@helmholtz-muenchen.de>
2022-11-28 16:51:45 +0100

    more colorblind palettes

--- xdbit_funcs/plotting/color_palettes.py
+++ xdbit_funcs/plotting/color_palettes.py
@@ -10,3 +10,9 @@ class color_palettes:
         
         # palette from Caro. Optimized for colorblind people.
         self.caro = ['#3288BD','#440055', '#D35F5F', '#A02C2C','#225500', '#66C2A5', '#447C69']
+        
+        # from https://thenode.biologists.com/data-visualization-with-flying-colors/research/
+        self.okabe_ito = ["#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#D55E00", "#CC79A7", "#000000"]
+        self.tol_bright = ["#EE6677", "#EE6677", "#4477AA", "#CCBB44", "#66CCEE", "#AA3377", "#BBBBBB"]
+        self.tol_muted = ["#88CCEE", "#44AA99", "#117733", "#332288", "#DDCC77", "#999933", "#CC6677", "#882255", "#AA4499", "#DDDDDD"]
+        self.tol_light = ["#BBCC33", "#AAAA00", "#77AADD", "#EE8866", "#EEDD88", "#FFAABB", "#99DDFF", "#44BB99", "#DDDDDD"]
